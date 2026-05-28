8091576f9b744392922d9856a199b49fa0ffb1f6
Зимбицкий Павел Игоревич <control.blago@gmail.com>
2026-05-28 17:52:23 +0300

    Реализовал систему парсинга команд: добавил классы Argument, Option, Command, ArgResults, HelpCommand и обновил CommandRunner

--- dartpedia/command_runner/lib/command_runner.dart
+++ dartpedia/command_runner/lib/command_runner.dart
@@ -1,4 +1,5 @@
-/// A simple command runner to handle command-line arguments.
 library;
 
+export 'src/arguments.dart';
 export 'src/command_runner_base.dart';
+export 'src/help_command.dart';
--- dartpedia/command_runner/lib/src/command_runner_base.dart
+++ dartpedia/command_runner/lib/src/command_runner_base.dart
@@ -1,5 +1,44 @@
+import 'dart:collection';
+import 'dart:io';
+import 'arguments.dart';
+
 class CommandRunner {
+  final Map<String, Command> _commands = <String, Command>{};
+
+  UnmodifiableSetView<Command> get commands =>
+      UnmodifiableSetView<Command>(<Command>{..._commands.values});
+
   Future<void> run(List<String> input) async {
-    print('CommandRunner received arguments: $input');
+    if (input.isEmpty) {
+      print(usage);
+      return;
+    }
+    
+    final ArgResults results = parse(input);
+    if (results.command != null) {
+      Object? output = await results.command!.run(results);
+      if (output != null) {
+        print(output.toString());
+      }
+    } else {
+      print('Unknown command: ${input.first}');
+      print(usage);
+    }
+  }
+
+  void addCommand(Command command) {
+    _commands[command.name] = command;
+    command.runner = this;
+  }
+
+  ArgResults parse(List<String> input) {
+    var results = ArgResults();
+    results.command = _commands[input.first];
+    return results;
+  }
+
+  String get usage {
+    final exeFile = Platform.script.path.split('/').last;
+    return 'Usage: dart bin/$exeFile <command> [commandArg?] [...options?]';
   }
 }
